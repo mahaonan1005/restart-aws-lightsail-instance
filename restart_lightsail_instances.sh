028f123222df63a129e1e8b3c252685a3362da92
mahaonan1005 <mahaonan1993@gmail.com>
2023-12-06 08:09:15 +0800

    Update restart_lightsail_instances.sh

--- restart_lightsail_instances.sh
+++ restart_lightsail_instances.sh
@@ -1,38 +1,57 @@
 #!/bin/bash
 
-# 获取所有静态 IP 地址
+# 定义一个函数来检查命令的执行状态
+check_status() {
+  if [ $? -ne 0 ]; then
+    echo "Error: $1 failed."
+    exit 1
+  fi
+}
+
+# 获取所有静态 IP 地址并解除实例关联
 aws lightsail get-static-ips --query 'staticIps[*].[name]' --output text | while read -r ip_name
 do
-  # 首先解除实例关联
-  instance=$(aws lightsail get-static-ip --static-ip-name $ip_name --query 'staticIp.attachedTo' --output text)
-  
+  # 获取与静态 IP 关联的实例
+  instance=$(aws lightsail get-static-ip --static-ip-name "$ip_name" --query 'staticIp.attachedTo' --output text)
+  check_status "Fetching static IP info for $ip_name"
+
+  # 如果 IP 已关联，则解除关联
   if [ "$instance" != "None" ]; then
     echo "Detaching $ip_name from $instance..."
-    aws lightsail detach-static-ip --static-ip-name $ip_name
+    aws lightsail detach-static-ip --static-ip-name "$ip_name"
+    check_status "Detaching $ip_name"
   fi
 
-  # 如果 IP 存在，则删除
+  # 删除静态 IP
   echo "Deleting $ip_name..."
-  aws lightsail release-static-ip --static-ip-name $ip_name
+  aws lightsail release-static-ip --static-ip-name "$ip_name"
+  check_status "Deleting $ip_name"
 done
 
-# Wait for 5 seconds
+# 等待操作完成
 sleep 5s
 
 # 获取所有实例名称
 instance_names=$(aws lightsail get-instances | jq -r '.instances[] | .name')
+check_status "Fetching instance names"
 
-# Stop instances
-echo "$instance_names" | xargs --no-run-if-empty -P 4 -I {} aws lightsail stop-instance --instance-name {}
+# 停止实例
+echo "$instance_names" | xargs --no-run-if-empty -P 4 -I {} bash -c '{
+  echo "Stopping instance {}..."
+  aws lightsail stop-instance --instance-name {} && echo "Stopped instance {}"
+}'
 
-# Wait for 70 seconds
+# 等待实例完全停止
 sleep 70s
 
-# Start instances
-echo "$instance_names" | xargs --no-run-if-empty -P 4 -I {} aws lightsail start-instance --instance-name {}
+# 启动实例
+echo "$instance_names" | xargs --no-run-if-empty -P 4 -I {} bash -c '{
+  echo "Starting instance {}..."
+  aws lightsail start-instance --instance-name {} && echo "Started instance {}"
+}'
 
-# Wait for 45 seconds
+# 等待实例启动
 sleep 45s
 
-# Display instance names and public IP addresses
+# 显示实例名称和公共 IP 地址
 aws lightsail get-instances --query "instances[*].[name, publicIpAddress]" --output json | jq -r '.[] | @tsv' | sort
